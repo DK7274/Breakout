a60ab8f23111c42fa5ca2a6f24321d302cc4eb16
Darcy <dkeys8722@gmail.com>
2022-07-24 19:37:16 +1000

    assigned bat and ball to images

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ backdrop = pygame.transform.scale(backdrop,(1000,1000)) #resizes image
 backdropbox = screen.get_rect()
 pygame.display.set_caption("Breaking Out") #names window
 
+#bat
+bat = pygame.image.load("Images/breakout.png").convert_alpha()
+bat_rect = bat.get_rect()
+#ball
+ball = pygame.image.load("Images/ball.png").convert_alpha()
+ball_rect = ball.get_rect
+#bricks
+
 clock = pygame.time.Clock()
 
 game_over = False
